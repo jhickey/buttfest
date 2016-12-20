7a244ab4a509df8ba4e854a5c18f8aad09b3b3cf
jhickey <jameshickey@me.com>
2016-12-19 19:19:58 -0500

    nicer dropdown

--- src/js/components/Graph.js
+++ src/js/components/Graph.js
@@ -1,5 +1,5 @@
 import  React, {Component} from 'react';
-import {Panel} from 'react-bootstrap';
+import {Panel, FormGroup, FormControl} from 'react-bootstrap';
 import {datadogGraphs} from '../constants';
 import DatadogGraphEmbed from './DataDogGraphEmbed';
 
@@ -23,11 +23,13 @@ export default class Grill extends Component {
       <Panel
         header="Temperature over time"
       >
-        <select onChange={::this.handleChangeGraph}>
-          <option value={datadogGraphs.LAST_HOUR}>1 Hour</option>
-          <option value={datadogGraphs.LAST_4_HOURS}>4 Hours</option>
-          <option value={datadogGraphs.LAST_DAY}>1 Day</option>
-        </select>
+        <FormGroup controlId="formControlsSelect">
+          <FormControl componentClass="select" onChange={::this.handleChangeGraph}>
+            <option value={datadogGraphs.LAST_HOUR}>1 Hour</option>
+            <option value={datadogGraphs.LAST_4_HOURS}>4 Hours</option>
+            <option value={datadogGraphs.LAST_DAY}>1 Day</option>
+          </FormControl>
+        </FormGroup>
         <DatadogGraphEmbed graph={currentGraph}/>
       </Panel>
     );
